a109536b48d1f5909e341d9716c3ceb9f0eef80a
duzy.chan <duzy.chan@463a7b20-940c-11dd-86f2-5bf34d4ec66d>
2009-08-19 03:08:08 +0000

    add builtins source
    git-svn-id: https://smart-make.googlecode.com/svn/branches/in-cpp@349 463a7b20-940c-11dd-86f2-5bf34d4ec66d

---
+++ src/builtins.cpp
@@ -0,0 +1,14 @@
+#include "vm_ops.hpp"
+#include "context.hpp"
+namespace smart
+{
+  namespace builtins
+  {
+
+    void expand_macro( vm::jit_info_t *ji, context *ctx )
+    {
+      
+    }
+
+  }//namespace builtins
+}//namespace smart
