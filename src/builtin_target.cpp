3f18da0069760b4220e4ec5ee904d99a7a73e61b
duzy.chan <duzy.chan@463a7b20-940c-11dd-86f2-5bf34d4ec66d>
2009-08-29 08:26:18 +0000

    $<
    git-svn-id: https://smart-make.googlecode.com/svn/branches/in-cpp@407 463a7b20-940c-11dd-86f2-5bf34d4ec66d

--- src/builtin_target.cpp
+++ src/builtin_target.cpp
@@ -106,6 +106,90 @@ namespace smart
       }
     }
 
+    static vm::type_string get_f( const vm::type_string & s )
+    {
+      vm::type_string f( s );
+      return f;
+    }
+
+    static vm::type_string get_d( const vm::type_string & s )
+    {
+      vm::type_string d( s );
+      return d;
+    }
+    
+    static void execute_commands( context & ctx, const target & tar, const make_rule & r )
+    {
+      vm::type_string empty;
+
+      builtin::macro m00( ctx.mtable()->map("@") );
+      builtin::macro m10( ctx.mtable()->map("%") );
+      builtin::macro m20( ctx.mtable()->map("<") );
+      builtin::macro m30( ctx.mtable()->map("?") );
+      builtin::macro m40( ctx.mtable()->map("^") );
+      builtin::macro m50( ctx.mtable()->map("+") );
+      builtin::macro m60( ctx.mtable()->map("|") );
+      builtin::macro m70( ctx.mtable()->map("*") );
+
+      builtin::macro m01( ctx.mtable()->map("@F") );
+      builtin::macro m11( ctx.mtable()->map("%F") );
+      builtin::macro m21( ctx.mtable()->map("<F") );
+      builtin::macro m31( ctx.mtable()->map("?F") );
+      builtin::macro m41( ctx.mtable()->map("^F") );
+      builtin::macro m51( ctx.mtable()->map("+F") );
+      builtin::macro m61( ctx.mtable()->map("|F") );
+      builtin::macro m71( ctx.mtable()->map("*F") );
+
+      builtin::macro m02( ctx.mtable()->map("@D") );
+      builtin::macro m12( ctx.mtable()->map("%D") );
+      builtin::macro m22( ctx.mtable()->map("<D") );
+      builtin::macro m32( ctx.mtable()->map("?D") );
+      builtin::macro m42( ctx.mtable()->map("^D") );
+      builtin::macro m52( ctx.mtable()->map("+D") );
+      builtin::macro m62( ctx.mtable()->map("|D") );
+      builtin::macro m72( ctx.mtable()->map("*D") );
+	  
+      m00.set_value( tar.object() );
+      m01.set_value( get_f(tar.object()) );
+      m02.set_value( get_d(tar.object()) );
+      m10.set_value( empty );
+      m11.set_value( empty );
+      m12.set_value( empty );
+      if ( 0 < r.prerequisites().size() ) {
+	vm::type_string preq( r.prerequisites()[0].object() );
+	m20.set_value( preq );
+	m21.set_value( get_f(preq) );
+	m22.set_value( get_d(preq) );
+      }
+
+      r.execute_commands( ctx );
+
+      m00.set_value(empty);
+      m10.set_value(empty);
+      m20.set_value(empty);
+      m30.set_value(empty);
+      m40.set_value(empty);
+      m50.set_value(empty);
+      m60.set_value(empty);
+      m70.set_value(empty);
+      m01.set_value(empty);
+      m11.set_value(empty);
+      m21.set_value(empty);
+      m31.set_value(empty);
+      m41.set_value(empty);
+      m51.set_value(empty);
+      m61.set_value(empty);
+      m71.set_value(empty);
+      m02.set_value(empty);
+      m12.set_value(empty);
+      m22.set_value(empty);
+      m32.set_value(empty);
+      m42.set_value(empty);
+      m52.set_value(empty);
+      m62.set_value(empty);
+      m72.set_value(empty);
+    }//execute_commands
+
     target::update_result target::update( context & ctx ) const
     {
       update_result uc = {0, 0, 0};
@@ -129,63 +213,7 @@ namespace smart
       if ( isPhony || 0 < uc.count_updated || 0 < uc.count_newer || lastWriteTime == 0 /*|| !this->exists()*/ ) {
 	bool b( lastWriteTime == 0 || 0 < uc.count_newer );
 
-	{
-	  builtin::macro m0( ctx.mtable()->map("@") );
-	  builtin::macro m1( ctx.mtable()->map("%") );
-	  builtin::macro m2( ctx.mtable()->map("<") );
-	  builtin::macro m3( ctx.mtable()->map("?") );
-	  builtin::macro m4( ctx.mtable()->map("^") );
-	  builtin::macro m5( ctx.mtable()->map("+") );
-	  builtin::macro m6( ctx.mtable()->map("|") );
-	  builtin::macro m7( ctx.mtable()->map("*") );
-	  m0.set_value( _i->_object );
-
-	  builtin::macro m01( ctx.mtable()->map("@F") );
-	  builtin::macro m11( ctx.mtable()->map("%F") );
-	  builtin::macro m21( ctx.mtable()->map("<F") );
-	  builtin::macro m31( ctx.mtable()->map("?F") );
-	  builtin::macro m41( ctx.mtable()->map("^F") );
-	  builtin::macro m51( ctx.mtable()->map("+F") );
-	  builtin::macro m61( ctx.mtable()->map("|F") );
-	  builtin::macro m71( ctx.mtable()->map("*F") );
-
-	  builtin::macro m02( ctx.mtable()->map("@D") );
-	  builtin::macro m12( ctx.mtable()->map("%D") );
-	  builtin::macro m22( ctx.mtable()->map("<D") );
-	  builtin::macro m32( ctx.mtable()->map("?D") );
-	  builtin::macro m42( ctx.mtable()->map("^D") );
-	  builtin::macro m52( ctx.mtable()->map("+D") );
-	  builtin::macro m62( ctx.mtable()->map("|D") );
-	  builtin::macro m72( ctx.mtable()->map("*D") );
-	  
-	  r.execute_commands( ctx );
-
-	  vm::type_string empty;
-	  m0.set_value(empty);
-	  m1.set_value(empty);
-	  m2.set_value(empty);
-	  m3.set_value(empty);
-	  m4.set_value(empty);
-	  m5.set_value(empty);
-	  m6.set_value(empty);
-	  m7.set_value(empty);
-	  m01.set_value(empty);
-	  m11.set_value(empty);
-	  m21.set_value(empty);
-	  m31.set_value(empty);
-	  m41.set_value(empty);
-	  m51.set_value(empty);
-	  m61.set_value(empty);
-	  m71.set_value(empty);
-	  m02.set_value(empty);
-	  m12.set_value(empty);
-	  m22.set_value(empty);
-	  m32.set_value(empty);
-	  m42.set_value(empty);
-	  m52.set_value(empty);
-	  m62.set_value(empty);
-	  m72.set_value(empty);
-	}
+	execute_commands( ctx, *this, r );
 
 	if ( b /*&& this->exists()*/ ) {
 	  if ( lastWriteTime < this->last_write_time() )
